ba214c140de8f638c903e050ce69d32cbed6df43
AliJalalifar <30731146+AliJalalifar@users.noreply.github.com>
2017-12-26 22:05:45 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def F_extract(W,c=1,ah=-0.1,at=0,bh=0,bt=0):
 # Encoding X input | feature space -> hidden space
 def Motion_Edit(hs_vector,encoder_path):
     loaded_model = load_Model(encoder_path)
-    Xinput = Input(shape=(240,70))
+    Xinput = Input(shape=(240,73))
     Conv_layer  = loaded_model.layers[1](Xinput)
     Pooling  = loaded_model.layers[2](Conv_layer)
     encoder = Model(Xinput,Pooling)
